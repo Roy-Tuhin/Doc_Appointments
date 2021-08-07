47eeda640fa5760baab0cc96f4233eaf557d5340
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-08-08 01:54:25 +0530

    up and running

--- lib/main.dart
+++ lib/main.dart
@@ -19,23 +19,16 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: 'Medbo',
-      // theme: ThemeData(
-      //   primaryColor: AppColor.colorPrimary1,
-      //   accentColor: AppColor.colorPrimary2,
-      // ),
-      // home: Home(),
-      // initialRoute: '/',
-      // routes: {
-      //   '/': (_) => Home(),
-      //   '/services': (_) => Services(),
-      //   '/about': (_) => About(),
-      //   // '/blogs':(_)=>Blogs(),
-      //   '/settings': (_) => Settings(),
-      //   '/splash': (_) => SplashScreen(),
-      // },
-      home: SplashScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => Home(),
+        '/services': (_) => Services(),
+        '/about': (_) => About(),
+        // '/blogs':(_)=>Blogs(),
+        '/settings': (_) => Settings(),
+        '/splash': (_) => SplashScreen(),
+      },
+
     );
   }
 }
-
-Can you please share it to your github repository? I would like to check it on my Android Studio.ok email please. ashiqullahmg@gmail.com //ok sendng you in a minute? bye for now? no. I am waiting for you email.yea wait// hey i did not creat repo in git hub for this.. cresting and sending you the link
