cfff4519acd3ad5dff7ef27c02cec480bf225c74
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-09-16 20:57:30 +0530

    validation added on text field

--- lib/Booking/PathologicalMultipleBooking/SingleTestBooking.dart
+++ lib/Booking/PathologicalMultipleBooking/SingleTestBooking.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
- import 'package:file_picker/file_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
@@ -17,7 +17,7 @@ import 'package:async/async.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-import 'package:http_parser/http_parser.dart'; 
+import 'package:http_parser/http_parser.dart';
 
 class PathologicalSingleTestBookingPage extends StatefulWidget {
   PathologicalSingleTestBookingPage() : super();
@@ -29,20 +29,21 @@ class PathologicalSingleTestBookingPage extends StatefulWidget {
 
 class _PathologicalSingleTestBookingPageState
     extends State<PathologicalSingleTestBookingPage> {
+  var _formKey = GlobalKey<FormState>();
   final ImagePicker _picker = ImagePicker();
   File? image;
 
-   var testTextController = TextEditingController();
+  var testTextController = TextEditingController();
 
   //=====================================================================================S H O W   USER  DETIALS IN APP DRAWER WITH SHARED PREFERENCES====================================================
 
   String Name = "";
   String EncUserId = "";
 
-void initState(){
-  super.initState();
-  getCred();
-}
+  void initState() {
+    super.initState();
+    getCred();
+  }
 
   void getCred() async {
     //HERE WE FETCH OUR CREDENTIALS FROM SHARED PREF
@@ -174,123 +175,132 @@ void initState(){
                   child: Column(
                     //mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [ 
-                                          ListTile(
-                        title: Text(
-                      "Required Tests",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: blockSizeHorizontal * 5,
-                        fontFamily: 'Poppins',
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    )),
-
-                    Padding(
-                      padding: const EdgeInsets.only(left: 13, right: 13),
-                      child: TextFormField(
-                        controller: testTextController,
+                    children: [
+                      ListTile(
+                          title: Text(
+                        "Required Tests",
                         style: TextStyle(
-                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: blockSizeHorizontal * 5,
                           fontFamily: 'Poppins',
+                          color: Theme.of(context).primaryColor,
                         ),
-                        //controller: searchController,
-                        onChanged: (text) {
-                          setState(
-                            () {
-                              // textLength = text.length;
+                      )),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13, right: 13),
+                        child: Form(
+                          key: _formKey,
+                          child: TextFormField(
+                            controller: testTextController,
+                            validator: (String? value) {
+                              if (value!.isEmpty) {
+                                return "Please Enter Test name";
+                              }
                             },
-                          );
-                        },
-
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black12),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(20),
-                              bottomRight: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                              topLeft: Radius.circular(20),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
                             ),
-                          ),
+                            //controller: searchController,
+                            onChanged: (text) {
+                              setState(
+                                () {
+                                  // textLength = text.length;
+                                },
+                              );
+                            },
 
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors
-                                    .black12 //greenAccent//Color(0xFF425c5a),
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20),
                                 ),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(20),
-                              bottomRight: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                              topLeft: Radius.circular(20),
+                              ),
+
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black12),
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20),
+                                ),
+                              ),
+
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20),
+                                ),
+                              ),
+
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20),
+                                ),
+                              ),
+
+                              // suffixIcon: IconButton(
+                              // icon: Icon(Icons.search, color: Colors.white,size: 34,),
+                              // onPressed: (){
+                              //   //searchByUser();
+                              //   },//=============================================================
+                              // ),
+
+                              // suffixIcon: Icon(textLength <= 5 ? Icons.cancel : Icons.check,
+                              //     color: textLength >= 6 ? Colors.green : Colors.redAccent),
+
+                              labelText: 'Test Name',
+                              labelStyle: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Colors.blueGrey, //Color(0xFF425c5a),
+                                fontWeight: FontWeight.normal,
+                                letterSpacing: 2.0,
+                                fontSize: 11.0,
+                              ),
+                              //hintText: 'Ex : Dental or Sugar Check up etc',
+                              hintStyle: TextStyle(
+                                fontFamily: 'Poppins',
+                                color: Colors.white38, //Color(0xFF425c5a),
+                                fontWeight: FontWeight.normal,
+                                letterSpacing: 2.0,
+                                fontSize: 9.0,
+                              ),
+                              //filled: true,
+                              //fillColor: Colors.grey[200],
+                              prefixIcon: Icon(
+                                Icons.edit,
+                                color: Colors.blueGrey,
+                              ),
                             ),
-                          ),
 
-                          errorBorder: OutlineInputBorder(
-                            //  borderSide: BorderSide(color: Colors.red[100]),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(20),
-                              bottomRight: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                              topLeft: Radius.circular(20),
-                            ),
-                          ),
+                            //keyboardType: TextInputType.number,
+                            // inputFormatters: [
+                            //   //only numeric keyboard.
+                            //  // LengthLimitingTextInputFormatter(6), //only 6 digit
+                            //  // WhitelistingTextInputFormatter.digitsOnly,
+                            // ],
 
-                          // suffixIcon: IconButton(
-                          // icon: Icon(Icons.search, color: Colors.white,size: 34,),
-                          // onPressed: (){
-                          //   //searchByUser();
-                          //   },//=============================================================
-                          // ),
-
-                          // suffixIcon: Icon(textLength <= 5 ? Icons.cancel : Icons.check,
-                          //     color: textLength >= 6 ? Colors.green : Colors.redAccent),
-
-                          labelText: 'Test Name',
-                          labelStyle: TextStyle(
-                            fontFamily: 'Poppins',
-                            color: Colors.blueGrey, //Color(0xFF425c5a),
-                            fontWeight: FontWeight.normal,
-                            letterSpacing: 2.0,
-                            fontSize: 11.0,
-                          ),
-                          //hintText: 'Ex : Dental or Sugar Check up etc',
-                          hintStyle: TextStyle(
-                            fontFamily: 'Poppins',
-                            color: Colors.white38, //Color(0xFF425c5a),
-                            fontWeight: FontWeight.normal,
-                            letterSpacing: 2.0,
-                            fontSize: 9.0,
-                          ),
-                          //filled: true,
-                          //fillColor: Colors.grey[200],
-                          prefixIcon: Icon(
-                            Icons.face,
-                            color: Colors.blueGrey,
+                            // validator: (String value) {
+                            //   if (value.length < 6) {
+                            //     return 'Enter your vaild 6 digit User ID';
+                            //   }
+                            //   return null;
+                            // },                                                                              // Active button validation
                           ),
                         ),
-
-                        //keyboardType: TextInputType.number,
-                        // inputFormatters: [
-                        //   //only numeric keyboard.
-                        //  // LengthLimitingTextInputFormatter(6), //only 6 digit
-                        //  // WhitelistingTextInputFormatter.digitsOnly,
-                        // ],
-
-                        // validator: (String value) {
-                        //   if (value.length < 6) {
-                        //     return 'Enter your vaild 6 digit User ID';
-                        //   }
-                        //   return null;
-                        // },                                                                              // Active button validation
                       ),
-                    ),
-
-
-
-
-
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: ListTile(
@@ -318,22 +328,32 @@ void initState(){
                       ),
                       Padding(
                         padding: const EdgeInsets.all(18.0),
-                        child: image == null? Text("No file chosen"): Image.file(File(image!.path),width: 150,fit: BoxFit.cover,),
+                        child: image == null
+                            ? Text("No file chosen")
+                            : Image.file(
+                                File(image!.path),
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
                       ),
-
-                     Center(
-                       child: ElevatedButton(
-                   onPressed: (){
-                       SaveCustomTestBooking();
-                 },
-
-                   style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Color(0xFF79a0be)),
-                                 padding: MaterialStateProperty.all(EdgeInsets.only(left:50,right: 50)),
-                          ), 
-                 child: Text("Next")),
-                     ),
+                      Center(
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (_formKey.currentState!.validate()) {
+                                  SaveCustomTestBooking();
+                                }
+                              });
+                              //SaveCustomTestBooking();
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Color(0xFF79a0be)),
+                              padding: MaterialStateProperty.all(
+                                  EdgeInsets.only(left: 50, right: 50)),
+                            ),
+                            child: Text("Next")),
+                      ),
                     ],
                   ),
                 ),
@@ -346,7 +366,8 @@ void initState(){
   }
 
   void filePicker() async {
-    final File? selectedImage =await ImagePicker.pickImage(source: ImageSource.gallery);
+    final File? selectedImage =
+        await ImagePicker.pickImage(source: ImageSource.gallery);
     print(selectedImage!.path);
     setState(() {
       image = selectedImage;
@@ -366,19 +387,24 @@ void initState(){
             'VisitDate': _selectedDate,
             'EncUserId': EncUserId,
             //'UserFile':  image != null ? 'data:image/png;base64,' + base64Encode(image!.readAsBytesSync()) : '',
-            'Description':testTextController.text
+            'Description': testTextController.text
           }));
       if (response.statusCode == 200) {
         print("Correct");
         print(response.body);
         jsonResponse = json.decode(response.body.toString());
         print(jsonResponse);
-        Navigator.push(context, MaterialPageRoute(builder: (context) => DieticianAfterDateSelectPage(rresponse:DieticianEncBookingIdModel.fromJson(jsonResponse),)));
-       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("Somthing went wrong")));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DieticianAfterDateSelectPage(
+                      rresponse:
+                          DieticianEncBookingIdModel.fromJson(jsonResponse),
+                    )));
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Somthing went wrong")));
         throw Exception("Faild to fetch");
-
       }
     }
   }
